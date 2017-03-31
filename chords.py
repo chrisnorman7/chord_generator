38701b0473068d6f1ccc06bd0133b6be3e24a4d5
Chris Norman <chris.norman2@googlemail.com>
2017-03-31 22:02:52 +0100

    Modernised the code.

--- chords.py
+++ chords.py
@@ -1,149 +1,224 @@
-#!/usr/bin/python
-from sys import argv
-from string import split
+"""
+A braille chord generator.
+"""
 
-config = {
-"chord_name" : ",blank ,*ord ,template",
-"neck_length" : 7,
-"auto_size" : 0,
-"show_brief" : 1,
-"brief_brief" : 1,
-"brief_delimiter" : "4",
-"brief_seperator" : "-",
-"fingers" : ["#j", "#a", "#b", "#c", "#d"],
-"strings" : [";e", ";b", ";g", ";d", ";a", ";e"],
-"empty_fret" : "33",
-"nonempty_fret" : "_",
-"normal_fret" : "=",
-"pre_nonempty_fret" : "l",
-"empty_string" : ";x",
-"silent_string" : "==",
-"input_seperator" : ".",
-"linebreak" : ""
-}
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from attr import attrs, attrib, Factory
 
-help_msg = "Syntax: " + argv[0] + " [--option1=value[ --option2=value2[ ...]]] [string[:fret:finger][ string[:fret:finger][ ...]]]\n\nOptions must be provided as --option=value, and for a list of all possible configuration directives with their current values, use --config.\nAll arguments must be numbers. For example 5:2:4, would place the 4th finger on the 2nd fret of the 5th string."
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
-neck = {}
+parser.add_argument(
+    '-n',
+    '--name',
+    default=',blank ,*ord ,template',
+    help='The name of the chord'
+)
+parser.add_argument(
+    '-b',
+    '--hide-brief',
+    action='store_false',
+    help='Hide the brief chord display'
+)
+parser.add_argument(
+    '-X',
+    '--extended-brief',
+    action='store_true',
+    help='Show finger names as well as fret numbers in the brief chord display'
+)
+parser.add_argument(
+    '-l',
+    '--length',
+    type=int,
+    default=None,
+    help='The length of the neck'
+)
+parser.add_argument(
+    '-D',
+    '--delimiter',
+    default='4',
+    help='The character to put between the fret number and the finger name in '
+    'the brief display'
+)
+parser.add_argument(
+    '-S',
+    '--separator',
+    default='-',
+    help='The character which should separate strings in the brief display'
+)
+parser.add_argument(
+    '--fingers',
+    default='#j,#a,#b,#c,#d',
+    help='A comma-separated list of finger names'
+)
+parser.add_argument(
+    '--any-finger',
+    metavar='STRING',
+    default='#x',
+    help='The text to be printed when no finger is provided in a marking'
+)
+parser.add_argument(
+    '-s',
+    '--strings',
+    default=';e,;b,;g,;d,;a,;e',
+    help='String names'
+)
+parser.add_argument(
+    '--empty',
+    metavar='STRING',
+    default='33',
+    help='The text to print for an empty fret'
+)
+parser.add_argument(
+    '--fingered',
+    metavar='STRING',
+    default='_',
+    help='The text to print after a nonempty fret'
+)
+parser.add_argument(
+    '--normal',
+    metavar='STRING',
+    default='=',
+    help='The text to print after an empty fret'
+)
+parser.add_argument(
+    '--pre-fingered',
+    metavar='STRING',
+    default='l',
+    help='The text to print before a finger marking'
+)
+parser.add_argument(
+    '--empty-string',
+    metavar='STRING',
+    default=';x',
+    help='The text to print at the beginning of a string with no finger '
+    'markings'
+)
+parser.add_argument(
+    '--muted-string',
+    metavar='STRING',
+    default='==',
+    help='The text to print before a string which should not be played'
+)
+parser.add_argument(
+    'markings',
+    nargs='*',
+    help='The finger markings to be printed in the form string[.fret.finger]. '
+    'If fret and finger are omitted the string will be muted.'
+)
 
-def convert_number(n):
-  numbers = ["j", "a", "b", "c", "d", "e", "f", "g", "h", "i"]
-  new_string = ""
-  for t in str(n):
-    new_string += numbers[int(t)]
-  return new_string.rjust(2, "#")
+args = parser.parse_args()
 
-for a in argv[1:]:
-  if a == "-H" or a == "--help":
-    quit(help_msg)
-  elif a == "--config":
-    for k in config.keys():
-      if type(config[k]) == list:
-        actual_string = ""
-        for v in config[k]:
-          actual_string += v + ", "
-          actual_string = actual_string[0:-1]
-      else:
-        actual_string = str(config[k])
-      print(k + "=" + actual_string)
-    quit()
-  elif a.startswith("--"):
-    a = a.split("=")
-    if len(a) != 2:
-      quit("Configuration options must be passed with --option=value.")
-    [option, value] = a
-    option = option[2:]
-    if not config.has_key(option):
-      quit("Invalid configuration directive: --" + option + ". See --config for a list.")
-    func = type(config[option])
-    if func == list:
-      config[option] = split(value, ",")
-    else:
-      try:
-        config[option] = func(value)
-      except ValueError:
-        quit("Invalid value for --" + option + ": " + value + ". Option expects value of " + str(type(config[option])) + ".")
-    continue
-  else:
-    a = split(a, config["input_seperator"])
-    for r in range(0,len(a)):
-      try:
-        a[r] = int(a[r])
-      except ValueError:
-        quit(help_msg)
-    if len(a) == 3:
-      [arg_string, arg_fret, arg_finger] = a
-    elif len(a) == 1:
-      [arg_string, arg_fret, arg_finger] = [a[0], 0, 0]
-    else:
-      quit(help_msg)
-    arg_string -= 1
-    if not (arg_string in range(0,len(config["strings"]))):
-      quit("This instrument is only configured for " + str(len(config["strings"])) + " strings. Thus " + str(arg_string + 1) + " is out of range.")
-    elif not (arg_finger in range(0, len(config["fingers"]))):
-      quit("Only " + str(len(config["fingers"])) + " fingers are named. These are numbered from 0 to " + str(len(config["fingers"]) - 1) + ". " + str(arg_finger) + " is invalid.")
-    elif not (arg_fret in range(0,config["neck_length"] + 1)) and not config["auto_size"]:
-      quit("This instrument only has " + str(config["neck_length"]) + " frets. " + str(arg_fret) + " is out of range.")
-    if not neck.has_key(arg_string):
-      neck[arg_string] = {}
-    if not arg_fret:
-      neck[arg_string][0] = config["silent_string"]
-    else:
-      arg_finger = config["fingers"][arg_finger].rjust(2, "#")
-      neck[arg_string][arg_fret - 1] = arg_finger
-      try:
-        if neck[arg_string][0] == config["empty_string"]:
-          del neck[arg_string][0]
-      except:
-        pass
 
-print(config["chord_name"])
+def convert_numbers(n):
+    """Returns the number n as a braille string."""
+    res = '#'
+    for x in str(n):
+        res += 'jabcdefghi'[int(x)]
+    return res
 
-if config["auto_size"]:
-  config["neck_length"] = 0
-  for n in neck:
-    for f in neck[n].keys():
-      config["neck_length"] = max(config["neck_length"], f + 1)
 
-line = ""
-if config["show_brief"]:
-  for counter in range(0, len(config["strings"])):
-    if not neck.has_key(counter):
-      line += config["empty_string"]
-    else:
-      for k in neck[counter].keys():
-        if neck[counter][k] == config["silent_string"]:
-          line += config["silent_string"]
-        else:
-          line += convert_number(k + 1)
-          if not config["brief_brief"]:
-            line += config["brief_delimiter"] + neck[counter][k]
-    line += config["brief_seperator"]
-  line = line[0:-1]
-print(line + config["linebreak"])
+@attrs
+class String:
+    """A string on an instrument."""
+    name = attrib()
+    muted = attrib(default=Factory(lambda: False))
+    markings = attrib(default=Factory(dict))
 
-line = "  "
-for r in range(1,config["neck_length"] + 1):
-  line += " " + convert_number(r)
-print(line)
+    def __str__(self):
+        return self.name
 
-for counter in range(0,len(config["strings"])):
-  line = config["strings"][counter].rjust(2, ";").ljust(3, " ")
-  for n in range(0,config["neck_length"]):
-    if n == 0 and not neck.has_key(counter):
-      fret = config["empty_string"]
-    else:
-      try:
-        fret = neck[counter][n]
-      except:
-        fret = config["empty_fret"]
-    line += fret
-    if not fret in [config["empty_fret"], config["silent_string"]]:
-      line += config["nonempty_fret"]
-      continue
+
+@attrs
+class Marking:
+    """A finger marking."""
+    finger = attrib()
+
+
+def splitter(string, fret=None, finger=None):
+    """returns a user-provided finger marking as a tuple of
+    (string, fret, finger)
+    ."""
+    return (string, fret, finger)
+
+
+strings = []
+for s in args.strings.split(','):
+    strings.append(String(s))
+
+args.fingers = args.fingers.split(',')
+for m in args.markings:
     try:
-      if neck[counter][n + 1]:
-        line += config["pre_nonempty_fret"]
-    except:
-      line += config["normal_fret"]
-  print(line + config["linebreak"])
+        res = m.split('.')
+        string, fret, finger = splitter(*res)
+        try:
+            string = int(string)
+        except ValueError:
+            raise ValueError('Invalid string number: %r.' % string)
+        if finger is not None:
+            try:
+                finger = int(finger)
+            except ValueError:
+                raise ValueError('Invalid finger number: %r.' % finger)
+        try:
+            s = strings[string - 1]
+        except IndexError:
+            raise ValueError('Invalid string number: {}.'.format(string))
+        try:
+            fret = int(fret)
+        except TypeError:
+            # fret is None, let's mute the string.
+            s.muted = True
+            continue
+        except ValueError:
+            raise ValueError('Invalid fret number: %r.' % fret)
+        if args.length is None:
+            args.length = fret
+        else:
+            args.length = max(args.length, fret)
+        try:
+            f = args.fingers[finger]
+        except TypeError:
+            # finger is None, use any finger.
+            f = args.any_finger
+        except IndexError:
+            raise ValueError('Invalid finger number: {}.'.format(finger))
+        s.markings[fret] = Marking(f)
+    except ValueError as e:
+        quit(e)
+
+
+if args.length is None:
+    args.length = 5  # There are no finger markings.
+
+print(args.name)
+
+res = '   '
+for x in range(args.length):
+    res = '%s%-3s' % (res, convert_numbers(x + 1))
+print(res)
+
+for s in strings:
+    if 1 not in s.markings:
+        if s.muted:
+            s.markings[1] = Marking(args.muted_string)
+        elif not s.markings:
+            s.markings[1] = Marking(args.empty_string)
+    res = ''
+    for x in range(args.length):
+        fret = x + 1
+        m = s.markings.get(fret)  # The current marking.
+        if m is None:
+            res = '%s%s%s' % (
+                res,
+                args.empty,
+                args.normal
+            )
+        else:
+            if x:
+                res = '%s%s' % (res[:-1], args.pre_fingered)
+            res = '%s%s%s' % (
+                res,
+                m.finger,
+                args.fingered
+            )
+    print('%s %s' % (s, res))
