49b921803629562ecceccee258f14b8ea2626716
Chris Norman <chris.norman2@googlemail.com>
2017-04-03 18:15:08 +0100

    Add all arguments to a list for easier parsing.

--- chords.py
+++ chords.py
@@ -27,132 +27,132 @@ parser = ArgumentParser(
     formatter_class=ArgumentDefaultsHelpFormatter,
 )
 
-parser.add_argument(
-    '--examples',
-    action='store_true',
-    help='Show usage examples'
-)
-
-parser.add_argument(
-    '-n',
-    '--name',
-    default=',blank ,*ord ,template',
-    help='The name of the chord'
-)
-parser.add_argument(
-    '-l',
-    '--length',
-    type=int,
-    default=0,
-    help='The length of the neck'
-)
-parser.add_argument(
-    '-b',
-    '--hide-brief',
-    action='store_true',
-    help='Hide the brief chord display'
-)
-parser.add_argument(
-    '-X',
-    '--extended-brief',
-    action='store_true',
-    help='Show finger names as well as fret numbers in the brief chord display'
-)
-parser.add_argument(
-    '-D',
-    '--delimiter',
-    default='4',
-    help='The character to put between the fret number and the finger name in '
-    'the brief display'
-)
-parser.add_argument(
-    '-S',
-    '--separator',
-    default='-',
-    help='The character which should separate strings in the brief display'
-)
-parser.add_argument(
-    '--number-sign',
-    metavar='CHARACTER',
-    default='#',
-    help='The sign to precede any numbers'
-)
-parser.add_argument(
-    '--numbers',
-    default='jabcdefghi',
-    help='A list of number characters to be used as numbers'
-)
-parser.add_argument(
-    '--fingers',
-    default='#j,#a,#b,#c,#d',
-    help='A comma-separated list of finger names'
-)
-parser.add_argument(
-    '--any-finger',
-    metavar='STRING',
-    default='#x',
-    help='The text to be printed when no finger is provided in a marking'
-)
-parser.add_argument(
-    '-s',
-    '--strings',
-    default=';e,;a,;d,;g,;b,;e',
-    help='String names'
-)
-parser.add_argument(
-    '-p',
-    '--pad-fret-numbers',
-    default=' ',
-    metavar='CHARACTER',
-    help='The character to print before the fret numbers'
-)
-parser.add_argument(
-    '--empty',
-    metavar='STRING',
-    default='33',
-    help='The text to print for an empty fret'
-)
-parser.add_argument(
-    '--fingered',
-    metavar='STRING',
-    default='_',
-    help='The text to print after a nonempty fret'
-)
-parser.add_argument(
-    '--normal',
-    metavar='STRING',
-    default='=',
-    help='The text to print after an empty fret'
-)
-parser.add_argument(
-    '--pre-fingered',
-    metavar='STRING',
-    default='l',
-    help='The text to print before a finger marking'
-)
-parser.add_argument(
-    '--empty-string',
-    metavar='STRING',
-    default=';x',
-    help='The text to print at the beginning of a string with no finger '
-    'markings'
-)
-parser.add_argument(
-    '--muted-string',
-    metavar='STRING',
-    default='==',
-    help='The text to print before a string which should not be played'
-)
-parser.add_argument(
-    'markings',
-    nargs='*',
-    help='The finger markings to be printed. Expected as:'
-    'string[.fret[.finger]]'
-    'If finger is muted then the any finger string will be used. If fret and '
-    'finger are omitted the string will be muted.'
-)
-
-args = parser.parse_args()
+arguments = [
+    parser.add_argument(
+        '--examples',
+        action='store_true',
+        help='Show usage examples'
+    ),
+    parser.add_argument(
+        '-n',
+        '--name',
+        default=',blank ,*ord ,template',
+        help='The name of the chord'
+    ),
+    parser.add_argument(
+        '-l',
+        '--length',
+        type=int,
+        default=0,
+        help='The length of the neck'
+    ),
+    parser.add_argument(
+        '-b',
+        '--hide-brief',
+        action='store_true',
+        help='Hide the brief chord display'
+    ),
+    parser.add_argument(
+        '-X',
+        '--extended-brief',
+        action='store_true',
+        help='Show finger names as well as fret numbers in the brief chord '
+        'display'
+    ),
+    parser.add_argument(
+        '-D',
+        '--delimiter',
+        default='4',
+        help='The character to put between the fret number and the finger '
+        'name in the brief display'
+    ),
+    parser.add_argument(
+        '-S',
+        '--separator',
+        default='-',
+        help='The character which should separate strings in the brief display'
+    ),
+    parser.add_argument(
+        '--number-sign',
+        metavar='CHARACTER',
+        default='#',
+        help='The sign to precede any numbers'
+    ),
+    parser.add_argument(
+        '--numbers',
+        default='jabcdefghi',
+        help='A list of number characters to be used as numbers'
+    ),
+    parser.add_argument(
+        '--fingers',
+        default='#j,#a,#b,#c,#d',
+        help='A comma-separated list of finger names'
+    ),
+    parser.add_argument(
+        '--any-finger',
+        metavar='STRING',
+        default='#x',
+        help='The text to be printed when no finger is provided in a marking'
+    ),
+    parser.add_argument(
+        '-s',
+        '--strings',
+        default=';e,;a,;d,;g,;b,;e',
+        help='String names'
+    ),
+    parser.add_argument(
+        '-p',
+        '--pad-fret-numbers',
+        default=' ',
+        metavar='CHARACTER',
+        help='The character to print before the fret numbers'
+    ),
+    parser.add_argument(
+        '--empty',
+        metavar='STRING',
+        default='33',
+        help='The text to print for an empty fret'
+    ),
+    parser.add_argument(
+        '--fingered',
+        metavar='STRING',
+        default='_',
+        help='The text to print after a nonempty fret'
+    ),
+    parser.add_argument(
+        '--normal',
+        metavar='STRING',
+        default='=',
+        help='The text to print after an empty fret'
+    ),
+    parser.add_argument(
+        '--pre-fingered',
+        metavar='STRING',
+        default='l',
+        help='The text to print before a finger marking'
+    ),
+    parser.add_argument(
+        '--empty-string',
+        metavar='STRING',
+        default=';x',
+        help='The text to print at the beginning of a string with no finger '
+        'markings'
+    ),
+    parser.add_argument(
+        '--muted-string',
+        metavar='STRING',
+        default='==',
+        help='The text to print before a string which should not be played'
+    ),
+    parser.add_argument(
+        'markings',
+        nargs='*',
+        help='The finger markings to be printed. Expected as:'
+        'string[.fret[.finger]]'
+        'If finger is muted then the any finger string will be used. If fret '
+        'and finger are omitted the string will be muted.'
+    )
+]
 
 
 def convert_numbers(n):
@@ -188,58 +188,56 @@ def splitter(string, fret=None, finger=None):
     return (string, fret, finger)
 
 
-# This variable will grow with the length of each string name allowing the code
-# to correctly align the fret numbers in the display.
-string_name_length = 0
-strings = []
-for s in args.strings.split(','):
-    string_name_length = max(string_name_length, len(s))
-    strings.append(String(s))
-
-args.fingers = args.fingers.split(',')
-for m in args.markings:
-    try:
-        res = m.split('.')
-        string, fret, finger = splitter(*res)
-        try:
-            string = int(string)
-        except ValueError:
-            raise ValueError('Invalid string number: %r.' % string)
-        if finger is not None:
-            try:
-                finger = int(finger)
-            except ValueError:
-                raise ValueError('Invalid finger number: %r.' % finger)
-        try:
-            s = strings[string - 1]
-        except IndexError:
-            raise ValueError('Invalid string number: {}.'.format(string))
-        try:
-            fret = int(fret)
-        except TypeError:
-            # fret is None, let's mute the string.
-            s.muted = True
-            continue
-        except ValueError:
-            raise ValueError('Invalid fret number: %r.' % fret)
-        if args.length < 1:
-            args.length = fret
-        else:
-            args.length = max(args.length, fret)
-        try:
-            f = args.fingers[finger]
-        except TypeError:
-            # finger is None, use any finger.
-            f = args.any_finger
-        except IndexError:
-            raise ValueError('Invalid finger number: {}.'.format(finger))
-        s.markings[fret] = Marking(f, fret)
-    except ValueError as e:
-        quit(e)
-
-
 def main():
     """Let's print some tabs!"""
+    # This variable will grow with the length of each string name allowing the
+    # code to correctly align the fret numbers in the display.
+    string_name_length = 0
+    strings = []
+    for s in args.strings.split(','):
+        string_name_length = max(string_name_length, len(s))
+        strings.append(String(s))
+
+    args.fingers = args.fingers.split(',')
+    for m in args.markings:
+        try:
+            res = m.split('.')
+            string, fret, finger = splitter(*res)
+            try:
+                string = int(string)
+            except ValueError:
+                raise ValueError('Invalid string number: %r.' % string)
+            if finger is not None:
+                try:
+                    finger = int(finger)
+                except ValueError:
+                    raise ValueError('Invalid finger number: %r.' % finger)
+            try:
+                s = strings[string - 1]
+            except IndexError:
+                raise ValueError('Invalid string number: {}.'.format(string))
+            try:
+                fret = int(fret)
+            except TypeError:
+                # fret is None, let's mute the string.
+                s.muted = True
+                continue
+            except ValueError:
+                raise ValueError('Invalid fret number: %r.' % fret)
+            if args.length < 1:
+                args.length = fret
+            else:
+                args.length = max(args.length, fret)
+            try:
+                f = args.fingers[finger]
+            except TypeError:
+                # finger is None, use any finger.
+                f = args.any_finger
+            except IndexError:
+                raise ValueError('Invalid finger number: {}.'.format(finger))
+            s.markings[fret] = Marking(f, fret)
+        except ValueError as e:
+            quit(e)
     if args.length < 1:
         args.length = 5  # There are no finger markings.
     print(args.name)
@@ -296,6 +294,7 @@ def main():
 
 
 if __name__ == '__main__':
+    args = parser.parse_args()
     if args.examples:
         print(examples.format(prog=sys.argv[0]))
     else:
